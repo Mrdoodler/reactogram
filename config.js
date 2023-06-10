6951efc561ff4b7dd139ee9002fc7b3f00831925
MUKUND <86561383+Mrdoodler@users.noreply.github.com>
2023-06-11 00:33:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGODB_URL: "mongodb+srv://cluster0.megph38.mongodb.net/",
+    MONGODB_URL: "mongodb+srv://reactogram:reactogram123@cluster0.ji4zxnl.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 
 }
